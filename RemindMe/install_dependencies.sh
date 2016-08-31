1f076d6cbea43d2cb216622e9d7fc602939ba076
prakhar <pmprakhargenius@gmail.com>
2016-08-31 23:59:45 +0530

    Added espeak functionality

---
+++ RemindMe/install_dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Installing Voice Engine..."
+sudo apt-get install espeak
+
+echo "Installing Talkey..."
+sudo pip install talkey
+
+echo "Install EasyGUI..."
+sudo pip install easygui
--- RemindMe/take_fucking_pills.py
+++ RemindMe/take_fucking_pills.py
@@ -5,29 +5,50 @@ import os
 import subprocess
 import easygui
 import time
+import sys
+import talkey
 
 remindAgain = False
 
 
-# Prompt for the first time
-response_01 = easygui.msgbox('Take your medicines. Else you will be bald one day !!','BATRA MEDICINES')
+Message = "It is time to take your pills"
+
+def playMessage(Message):
+	try:
+		# Voice Feature
+		tts = talkey.Talkey(
+					preferred_languages=['en'],
+					preferred_factor=80.0,
+				    	engine_preference=['espeak']
+			   	   )
+		tts.say(Message)
+	except Exception as e:
+		return 0
+
+	return 1
+
+if playMessage(Message):
+	# Prompts for the first time
+	response_01 = easygui.msgbox('Take your medicines. Else you will be bald one day !!','BATRA MEDICINES')
+else:
+	sys.exit()
 
 if response_01 == 'OK':
 	remindAgain = True
 
 
-# Reminds again
+#Reminds again
 def remindMe():
 	response_02 = easygui.ynbox('Did you take your medicines ?', 'REMINDER', ('Yes','No'))
 	
 	if response_02:
 		return 1
 	else:
-		time.sleep(300)
+		time.sleep(3)
 		remindMe()
 
 if remindAgain:
-	time.sleep(300)
+	time.sleep(3)
 	responseFinal = remindMe()
 
 	if responseFinal:

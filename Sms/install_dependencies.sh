a808613621a40f298428821f42679b7aa23871e0
prakhar <pmprakhargenius@gmail.com>
2016-09-03 00:04:00 +0530

    Env. variable settings update

--- Sms/install_dependencies.sh
+++ Sms/install_dependencies.sh
@@ -2,3 +2,9 @@
 
 echo "Installing  twilio-python..."
 sudo pip install twilio 
+
+echo "Setting Environment..."
+echo 'export Twilio_Sid=<twilio sid>' >> ~/.bashrc
+echo 'export Twilio_Token=<twilio token>' >> ~/.bashrc
+echo 'export Phone_To=<others number>' >> ~/.bashrc
+echo 'export Phone_From=<your twilio number>' >> ~/.bashrc
--- Sms/replyToSMS.py
+++ Sms/replyToSMS.py
@@ -1,14 +1,34 @@
+#!/usr/bin/env python
 #@uthor: Prakhar
 
 # importing packages
 from twilio.rest import TwilioRestClient
 import sys
 import os
+import subprocess
+from prettytable import PrettyTable
 
-# credentials
-ACCOUNT_SID = "<Enter you SID>" 
-AUTH_TOKEN = "<Enter your TOKEN>" 
- 
+try:
+	os.system('sh install_dependencies.sh')
+	os.system('source ~/.bashrc')
+	print 'Environment is set.'
+except Exception as e:
+	print e
+	sys.exit()
+
+
+# Information is set in environment
+try:
+	ACCOUNT_SID = os.environ.get('Twilio_Sid')
+	AUTH_TOKEN = os.environ.get('Twilio_Token')
+	PHONE_TO = os.environ.get('Phone_To')
+	PHONE_FROM = os.environ.get('Phone_From')
+except Exception as KeyError:
+	print '\nEnvironment variables are not set properly'
+	sys.exit()
+
+
+
 def getClient(a_sid,a_token):
 	try:
 		client = TwilioRestClient(a_sid, a_token)
@@ -18,14 +38,21 @@ def getClient(a_sid,a_token):
 	return client
 
 # Caller details
-send_to = "" # sender number
-send_from = "" # twilio number
-sms_text = "Hi"
+sms_text = ""
+
+# Display the settings
+table = PrettyTable(["Setting", "Value"])
+table.add_row(["Twilio Sid", ACCOUNT_SID])
+table.add_row(["Twilio Token", AUTH_TOKEN])
+table.add_row(["Calling to", PHONE_TO])
+table.add_row(["My number", PHONE_FROM])
+print table
+
 
 client = getClient(ACCOUNT_SID,AUTH_TOKEN)
 
 try:
-	client.messages.create(to=send_to,from_=send_from,body=sms_text,)
+	client.messages.create(to=PHONE_TO,from_=PHONE_FROM,body=sms_text,)
 except Exception as e:
 	print e
 	sys.exit()

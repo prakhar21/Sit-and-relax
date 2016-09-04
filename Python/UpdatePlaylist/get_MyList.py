eddfbf0f8453c78b0d1c6068c714195b3307caf3
Prakhar Mishra <pmprakhargenius@gmail.com>
2016-09-05 00:55:40 +0530

    Update get_MyList.py

--- Python/UpdatePlaylist/get_MyList.py
+++ Python/UpdatePlaylist/get_MyList.py
@@ -24,6 +24,7 @@ def setEnvironment():
 def getLinkLocation():
 	try:
 		LINK = os.environ.get('Playlist_Location')
+		print LINK
 	except Exception as e:
 		print e
 		sys.exit()
@@ -31,23 +32,50 @@ def getLinkLocation():
 	return LINK
 
 
+def makeDirectoryForStorage():
+	try:
+		os.system('mkdir Videos')
+		return 1
+	except Exception as e:
+		return 0
+
 def runtasks(LINK):
-	playlist = pafy.get_playlist(URL)
+
+	if makeDirectoryForStorage():
+		pass
+	else:
+		'Directory does not exist\n'
+		sys.exit()
+
+	try:
+		os.chdir('Videos/')
+		playlist = pafy.get_playlist(LINK)
 	
-	no_of_songs = len(playlist['items'])
+		no_of_songs = len(playlist['items'])
 
-	song_downloadable_link = []
-	# loop through songs and download best ones
-	for song_id in xrange(,no_of_songs):
-		song_attributes = playlist['items'][song_id]['pafy']
-		song_downloadable_link.append(song_attributes.getbest().url)
+		song_downloadable_link = []
+	
+		# loop through songs and fetch their URL
+		for song_id in range(0,no_of_songs-1):
+			song_attributes = playlist['items'][song_id]['pafy'].getbest()
+			song_attributes.download()
+	
+		os.chdir('..')
 
-	return song_downloadable_link
+		return 1
 
+	except Exception as e:
+		print 'Unable to Fetch Songs\n'
+		return 0
+		
 if __name__ == '__main__':
 	setEnvironment()
+
 	LINK_LOC = getLinkLocation()	
-	runtasks(LINK_LOC)
-	print 'Exiting...'
-	sys.exit()
+
+	if runtasks(LINK_LOC):
+		print 'Successful Exit...'
+		sys.exit()
+	else:
+		sys.exit()
 

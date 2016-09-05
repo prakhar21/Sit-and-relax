ecf87bbd7f7b14a47cb40c05c79688b7873468ee
prakhar <pmprakhargenius@gmail.com>
2016-09-06 00:36:07 +0530

    Installation

--- Python/UpdatePlaylist/install_dependencies.sh
+++ Python/UpdatePlaylist/install_dependencies.sh
@@ -2,12 +2,20 @@
 
 echo 'Installing Pafy...'
 sudo pip install pafy
-#wget -O youtube-pafy https://github.com/np1/pafy/tarball/master
-#cd youtube-pafy
-#python setup.py install
+
+echo 'Installing Convertor...'
+sudo apt-get install pacpl
+
+echo 'Installing PyUSB...'
+sudo pip install pyusb --pre
+
+#sudo apt-get install go-mtpfs
+#sudo apt-get install libmtp
+#sudo apt-get install mtpfs mtp-tools
 
 echo 'Setting Environment Variables...'
 echo "export Playlist_Location='https://www.youtube.com/watch?v=SjahQsqSLLY&index=1&list=PLINP9i2Sqbf-vCwVXXfEvJOTnco5GOjMf'" >> ~/.bashrc
+
 
 
 
